8ba1c9d60b2c11fb592cf33480de769a7971f1ea
陈经伟 <>
2019-03-19 17:35:25 +0800

    no message

---
+++ NSString+HtmlStyle.h
@@ -0,0 +1,17 @@
+//
+//  NSString+HtmlStyle.h
+//  HomeHealth
+//
+//  Created by 陈经伟 on 16/12/1.
+//  Copyright © 2016年 陈经伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HtmlStyle)
+
+
+- (NSString *)autoLayoutSize;
+
+
+@end
